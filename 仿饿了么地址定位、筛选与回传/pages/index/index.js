76bf832f15a311720f3cc3871280e9f8e2b83d60
huangxiujie <huangxiujie@126.com>
2017-03-01 21:25:10 +0800

    加载得到当前定位所在中文地址

--- 仿饿了么地址定位、筛选与回传/pages/index/index.js
+++ 仿饿了么地址定位、筛选与回传/pages/index/index.js
@@ -7,9 +7,33 @@
  * @link https://my.oschina.net/huangxiujie
  * @author 黄秀杰
  */
- 
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
+
 Page({
     data: {
-        location: '浙江省温州市'
+        location: '正在请求地址...'
+    },
+    onLoad: function () {
+        var qqmapsdk = new QQMapWX({
+            key: 'BJFBZ-ZFTHW-Y2HRO-RL2UZ-M6EC3-GMF4U'
+        });
+                var that = this;
+        // 调用接口
+        qqmapsdk.reverseGeocoder({
+            poi_options: 'policy=2',
+            get_poi: 1,
+            success: function(res) {
+                console.log(res);
+                that.setData({
+                    location: res.result.address
+                });
+            },
+            fail: function(res) {
+        //         console.log(res);
+            },
+            complete: function(res) {
+        //         console.log(res);
+            }
+        });
     }
 })
