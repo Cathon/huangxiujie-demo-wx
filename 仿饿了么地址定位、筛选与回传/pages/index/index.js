ea6647d504b326132bbaa000c24430da1624de63
huangxiujie <huangxiujie@126.com>
2017-03-01 21:22:39 +0800

    首页布局

---
+++ 仿饿了么地址定位、筛选与回传/pages/index/index.js
@@ -0,0 +1,15 @@
+/**
+ * 仿饿了么地址选择，实现了地址定位、筛选与回传
+ *
+ * 配套视频教程请移步微信->小程序->灵动课堂
+ * 图文教程发布于订阅号【huangxiujie85】
+ *
+ * @link https://my.oschina.net/huangxiujie
+ * @author 黄秀杰
+ */
+ 
+Page({
+    data: {
+        location: '浙江省温州市'
+    }
+})
